821151efd32cc717391d6d3125c61074e4b71f37
viveknathani <viveknathani2402@gmail.com>
2020-10-04 15:21:05 +0530

    Function to generate a 128-bit IV for CBC mode

---
+++ backend/algorithm/GetIV.hpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <string>
+#include <time.h>
+#include <random>
+
+std::string getIV()
+{
+    std::string result;
+    char possibleValues[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    srand(time(NULL));
+    for(int i = 0; i < 16; i++)
+        result += possibleValues[rand() % 16];
+    
+    std::random_shuffle(result.begin(), result.end());
+    return result;
+}
