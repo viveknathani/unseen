ef20dbe209ac118507fa72b7a83865cc7f7700d6
viveknathani <viveknathani2402@gmail.com>
2020-11-12 15:11:36 +0530

    Setup CORS and uploader

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const server = express();
-server.listen(4000,function(){
-    console.log('server started');
-});
+const uploader = require('express-fileupload');
+const paths = require('./api/api');
+const cors = require('cors');
+
+const app = express();
+app.use(uploader());
+app.use(cors());
+paths(app);
+
+app.listen(4000, () => console.log('Server started,.'));
